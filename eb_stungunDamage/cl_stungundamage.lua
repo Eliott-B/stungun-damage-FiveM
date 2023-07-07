7c769c5c49dfc82f45f86d6bdb5c71b89b777be6
Eliott-B <eliottb.info@gmail.com>
2023-07-07 13:36:08 +0200

    ⚡ | Script

---
+++ eb_stungunDamage/cl_stungundamage.lua
@@ -0,0 +1,4 @@
+CreateThread(function()
+	local stungun = "weapon_stungun"
+    SetWeaponDamageModifier(stungun, 0)
+end)
---
+++ eb_stungunDamage/fxmanifest.lua
@@ -0,0 +1,8 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Eliott-B'
+description 'Modify the stungun damage.'
+version '1.0.0'
+
+client_script "cl_stungundamage.lua"
